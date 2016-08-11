fef79ab513a5e20dfabfd43cefbfeb5801948109
mwalcott <m.walcott33@gmail.com>
2016-08-11 08:56:01 -0600

    added staging info. started work on my books section

--- site/web/app/themes/reviewexchange/functions.php
+++ site/web/app/themes/reviewexchange/functions.php
@@ -380,6 +380,139 @@ new Reviewer_Preferences();
 // Flush rewrite rules on plugin activation.
 add_action( 'after_switch_theme', array( 'Reviewer_Preferences', 'install' ) );
 
+
+/*
+ * Add custom endpoint that appears in My Account Page - WooCommerce 2.6
+ * Ref - https://gist.github.com/claudiosmweb/a79f4e3992ae96cb821d3b357834a005#file-custom-my-account-endpoint-php
+ */
+
+
+class My_Books  {
+
+	/**
+	 * Custom endpoint name.
+	 *
+	 * @var string
+	 */
+	public static $endpoint = 'my-books';
+
+	/**
+	 * Plugin actions.
+	 */
+	public function __construct() {
+		// Actions used to insert a new endpoint in the WordPress.
+		add_action( 'init', array( $this, 'add_endpoints' ) );
+		add_filter( 'query_vars', array( $this, 'add_query_vars' ), 0 );
+
+		// Change the My Accout page title.
+		add_filter( 'the_title', array( $this, 'endpoint_title' ) );
+
+		// Insering your new tab/page into the My Account page.
+		add_filter( 'woocommerce_account_menu_items', array( $this, 'new_menu_items' ) );
+		add_action( 'woocommerce_account_' . self::$endpoint .  '_endpoint', array( $this, 'endpoint_content' ) );
+	}
+
+	/**
+	 * Register new endpoint to use inside My Account page.
+	 *
+	 * @see https://developer.wordpress.org/reference/functions/add_rewrite_endpoint/
+	 */
+	public function add_endpoints() {
+		add_rewrite_endpoint( self::$endpoint, EP_ROOT | EP_PAGES );
+	}
+
+	/**
+	 * Add new query var.
+	 *
+	 * @param array $vars
+	 * @return array
+	 */
+	public function add_query_vars( $vars ) {
+		$vars[] = self::$endpoint;
+
+		return $vars;
+	}
+
+	/**
+	 * Set endpoint title.
+	 *
+	 * @param string $title
+	 * @return string
+	 */
+	public function endpoint_title( $title ) {
+		global $wp_query;
+
+		$is_endpoint = isset( $wp_query->query_vars[ self::$endpoint ] );
+
+		if ( $is_endpoint && ! is_admin() && is_main_query() && in_the_loop() && is_account_page() ) {
+			// New page title.
+			$title = __( 'My Books', 'woocommerce' );
+
+			remove_filter( 'the_title', array( $this, 'endpoint_title' ) );
+		}
+
+		return $title;
+	}
+
+	/**
+	 * Insert the new endpoint into the My Account menu.
+	 *
+	 * @param array $items
+	 * @return array
+	 */
+	public function new_menu_items( $items ) {
+		// Remove the logout menu item.
+		$logout = $items['customer-logout'];
+		unset( $items['customer-logout'] );
+		// Insert your custom endpoint.
+		$items[ self::$endpoint ] = __( 'My Books', 'woocommerce' );
+
+		// Insert back the logout item.
+		$items['customer-logout'] = $logout;
+
+		return $items;
+	}
+
+	/**
+	 * Endpoint HTML content.
+	 */
+	public function endpoint_content() { ?>
+
+		<div class="woocommerce-MyAccount-content">
+			
+			<h2>My Books</h2>
+
+			<?php 
+/*
+				$args = array(
+					'' => ,
+				);
+*/
+				acf_form();
+			?>
+			
+
+		</div>
+
+		<?php
+	}
+
+	/**
+	 * Plugin install action.
+	 * Flush rewrite rules to make our custom endpoint available.
+	 */
+	public static function install() {
+		flush_rewrite_rules();
+	}
+}
+
+new My_Books();
+
+// Flush rewrite rules on plugin activation.
+add_action( 'after_switch_theme', array( 'My_Books', 'install' ) );
+
+
+
 function dashboard_helper() {
 	
 	the_field('dashboard_intro', 'option');
